3af5a9c9522e87f38b9662b27455ddfcdbc6e7e9
tristanwagstaff <ajaxthetiny@gmail.com>
2015-12-28 23:06:49 -0700

    test

--- start.js
+++ start.js
@@ -9,9 +9,16 @@ function startRails(port) {
 	var out = fs.openSync('./out_' + 'rails lamp app' + '.log', 'a');
 	var err = fs.openSync('./out_' + 'rails lamp app' + '.log', 'a');
 	console.log("Stopping and starting rail app");
-		var child = spawn("ruby", ["~/RubyTest/bin/rails", "server", "-p", port], {//, "server -p " + port], {
+    var child = spawn("ruby", ["~/RubyTest/bin/bundle", "install"], {
 		detached: true,
 		stdio: ['ignore', out, err]
 	});
+    
+		child = spawn("ruby", ["~/RubyTest/bin/rails", "server", "-p", port], {//, "server -p " + port], {
+		detached: true,
+		stdio: ['ignore', out, err]
+	});
+    
+    
 	child.unref();
 }
