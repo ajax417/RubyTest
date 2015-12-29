deca816e8190b7bfb9cec759ed00c9f0a85ad95c
tristanwagstaff <ajaxthetiny@gmail.com>
2015-12-28 22:37:55 -0700

    setting up scripts

--- start.js
+++ start.js
@@ -1,14 +1,14 @@
 var sys = require('sys');
 var spawn = require('child_process').spawn;
 var exec = require('child_process').exec;
-startRails(,3000);
+startRails(3000);
 
 function puts(error, stdout, stderr) {sys.puts(stdout)}
-function startRails(element, port) {
-	var out = fs.openSync('./out_' + element + '.log', 'a');
-	var err = fs.openSync('./out_' + element + '.log', 'a');
-	console.log("Stopping and starting rail app " + element);
-		var child = spawn("ruby", ["/home/ajax/WebProject/Rails_in_action/" + element + "/bin/rails", "server", "-p", port], {//, "server -p " + port], {
+function startRails(port) {
+	var out = fs.openSync('./out_' + 'rails lamp app' + '.log', 'a');
+	var err = fs.openSync('./out_' + 'rails lamp app' + '.log', 'a');
+	console.log("Stopping and starting rail app");
+		var child = spawn("ruby", ["/home/ajax/github_ruby_test/bin/rails", "server", "-p", port], {//, "server -p " + port], {
 		detached: true,
 		stdio: ['ignore', out, err]
 	});
